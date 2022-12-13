7f4166fa47a263c9c033952d13be5ca82b1b4bac
IfMalthereturnawesome <malt996b@stud.kea.dk>
2022-12-13 16:54:35 +0100

    Production Branch With Custom URL for My Azurewebsite

--- settings.js
+++ settings.js
@@ -1,5 +1,3 @@
 
-export let SERVER_URL = "http://localhost:8080/api"
-export let AUTH_URL = "http://localhost:8080/api/auth"
-
-export let SPECIFIC_CAR_MODEL_URL = "http://localhost:8080/api/specific-car-model"
+export let SERVER_URL = "https://fleet-management-kwb.azurewebsites.net/api"
+export let AUTH_URL = "https://fleet-management-kwb.azurewebsites.net/api/auth"
