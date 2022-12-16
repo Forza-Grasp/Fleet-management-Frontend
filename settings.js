d4ea0356dbe1d094708db1206021f7cc48c62434
MBHNielsenX <mads.nielsen12@live.dk>
2022-12-16 12:08:15 +0100

    Fixet

--- settings.js
+++ settings.js
@@ -1,5 +1,3 @@
 
 export let SERVER_URL = "https://forzagraspfleetmanagementbackend.azurewebsites.net/api"
 export let AUTH_URL = "https://forzagraspfleetmanagementbackend.azurewebsites.net/api/auth"
-
-export let SPECIFIC_CAR_MODEL_URL = "http://localhost:8080/api/specific-car-model"
